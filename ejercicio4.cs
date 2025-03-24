a08edf0e1a07825f7ee841460e13bbe53a9907da
Angel Noh <angelgabrielnohcamargo@gmail.com>
2025-03-24 16:37:51 -0600

    Add files via upload

---
+++ ejercicio4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numeros = new int[5];
+
+        // Pedir al usuario que ingrese 5 números
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            Console.Write($"Ingresa el número {i + 1}: ");
+            numeros[i] = int.Parse(Console.ReadLine());
+        }
+
+        // Mostrar el arreglo original
+        Console.WriteLine("\nArreglo original:");
+        foreach (int num in numeros)
+        {
+            Console.Write(num + " ");
+        }
+
+        // Mostrar el arreglo invertido
+        Console.WriteLine("\n\nArreglo invertido:");
+        for (int i = numeros.Length - 1; i >= 0; i--)
+        {
+            Console.Write(numeros[i] + " ");
+        }
+
+        Console.WriteLine(); // Salto de línea final
+    }
+}
